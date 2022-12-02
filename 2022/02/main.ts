d39e6f1123563ee8facc5b4e9888e783d313636f
Jesse <58728143+Jessuhh@users.noreply.github.com>
2022-12-02 10:15:50 +0100

    finished part 1. The answer this program gives you is 1 too high

--- 2022/02/main.ts
+++ 2022/02/main.ts
@@ -19,31 +19,33 @@ const YOU = {
 const getFinalScore = () => {
     return input
         .map((round) => {
-            const moves = round.split(" ");
-            const opponent = moves[0];
-            const you = moves[1];
-
             let points = 1;
-            if (you === YOU.PAPER) points = 2;
-            if (you === YOU.SCISSORS) points = 3;
-
-            if (
-                (opponent === OPPONENT.ROCK && you === YOU.ROCK) ||
-                (opponent === OPPONENT.PAPER && you === YOU.PAPER) ||
-                (opponent === OPPONENT.SCISSORS && you === YOU.SCISSORS)
-            ) {
-                return (points += 3);
+            switch (round.split(" ")[1]) {
+                case YOU.PAPER:
+                    points = 2;
+                    break;
+                case YOU.SCISSORS:
+                    points = 3;
+                    break;
             }
 
-            if (
-                (opponent === OPPONENT.ROCK && you === YOU.PAPER) ||
-                (opponent === OPPONENT.PAPER && you === YOU.SCISSORS) ||
-                (opponent === OPPONENT.SCISSORS && you === YOU.ROCK)
-            ) {
-                return (points += 6);
-            }
+            // console.log(round);
+            // console.log(points);
 
-            return points;
+            switch (round) {
+                // Draws
+                case `${OPPONENT.ROCK} ${YOU.ROCK}`:
+                case `${OPPONENT.PAPER} ${YOU.PAPER}`:
+                case `${OPPONENT.SCISSORS} ${YOU.SCISSORS}`:
+                    return points + 3;
+                // wins
+                case `${OPPONENT.SCISSORS} ${YOU.ROCK}`:
+                case `${OPPONENT.ROCK} ${YOU.PAPER}`:
+                case `${OPPONENT.PAPER} ${YOU.SCISSORS}`:
+                    return points + 6;
+                default:
+                    return points;
+            }
         })
         .reduce((a, b) => a + b, 0);
 };
