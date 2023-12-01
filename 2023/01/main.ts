5b987ce149ecdfab9556099298627e95d279286f
Jesse <58728143+Jessuhh@users.noreply.github.com>
2023-12-01 09:38:20 +0100

    Day 1 part 2

--- 2023/01/main.ts
+++ 2023/01/main.ts
@@ -31,10 +31,10 @@ const puzzleOne = input
 
 const puzzleTwo = input
     .map((line) => {
-        let firstValue = Number.MAX_VALUE;
-        let firstIndex = Number.MAX_VALUE;
+        const numbersOnly = line.replaceAll("one", "o1e").replaceAll("two", "t2o").replaceAll("three", "t3e").replaceAll("four", "f4r").replaceAll("five", "f5e").replaceAll("six", "s6x").replaceAll("seven", "s7n").replaceAll("eight", "e8t").replaceAll("nine", "n9e");
 
-        const characters = line.split("");
+
+        const characters = numbersOnly.split("");
 
         const first = characters.find((character) => {
             const number = parseInt(character, 10);
@@ -43,24 +43,6 @@ const puzzleTwo = input
             }
         });
 
-        if (first) {
-            if (line.indexOf(first) < firstIndex) {
-                firstIndex = line.indexOf(first);
-                firstValue = parseInt(first);
-            }
-        }
-
-        ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].forEach((number, index) => {
-            if (line.indexOf(number) < firstIndex && line.indexOf(number) !== -1) {
-                firstValue = index + 1;
-                firstIndex = line.indexOf(number);
-            }
-        });
-
-        // Second digit, from the back
-        let secondValue = Number.MAX_VALUE;
-        let secondIndex = 0;
-
         const second = characters.findLast((character) => {
             const number = parseInt(character, 10);
             if (number) {
@@ -68,21 +50,10 @@ const puzzleTwo = input
             }
         });
 
-        if (second) {
-            if (line.lastIndexOf(second) > secondIndex) {
-                secondIndex = line.lastIndexOf(second);
-                secondValue = parseInt(second);
-            }
-        }
+        console.log(`${first}${second}`);
 
-        ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].forEach((number, index) => {
-            if (line.lastIndexOf(number) > secondIndex && line.lastIndexOf(number) !== -1) {
-                secondValue = index + 1;
-                secondIndex = line.lastIndexOf(number);
-            }
-        });
+        return parseInt(`${first}${second}`);
 
-        return parseInt(`${firstValue}${secondValue}`); 
     })
     .reduce((previousValue, currentValue) => {
         return previousValue + currentValue;
